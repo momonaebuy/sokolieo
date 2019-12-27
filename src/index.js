ed8280c68a55a26487fbac4ab7034367680f55e1
momonaebuy <momona@angineers.com>
2019-12-27 22:39:54 +0700

    addes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
 import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './store/reducers/rootReducer';
 import { Provider } from 'react-redux';
@@ -20,5 +19,5 @@ const store = createStore(rootReducer,
 );
 store.firebaseAuthIsReady.then(()=>{
   ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
-  serviceWorker.unregister();  
+ 
 })
